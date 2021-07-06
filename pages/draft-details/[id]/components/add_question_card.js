1a3d1b832884ecd8c12337d3d888a3e49e94b291
Auro <auro@smarttersstudio.com>
2021-07-06 14:54:57 +0530

    wip : question add

---
+++ pages/draft-details/[id]/components/add_question_card.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @createdBy Aurosmruti Das
+ * @email aurosmruti.das@gmail.com
+ * @description add_question_card.js
+ * @createdOn 06/07/21 2:15 pm
+ */
+
+0
+
+
+
+
---
+++ pages/draft-details/[id]/components/question_card.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @createdBy Aurosmruti Das
+ * @email aurosmruti.das@gmail.com
+ * @description question_card.js
+ * @createdOn 06/07/21 2:14 pm
+ */
+
+
+
